32c76a1dc17d49cceab62f60ca4158d25083aae0
Sahar <saharujan@gmail.com>
2024-03-04 19:01:59 -0500

    finalizing the model

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import torch
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 from sklearn.model_selection import train_test_split
-
+from multiclass_model import BlobModel
+from torch import nn
+from torchmetrics import Accuracy
+import requests
+from pathlib import Path
+from helper_functions import plot_predictions, plot_decision_boundary
 # Set the hyperparameters for data creation
 NUM_CLASSES = 4
 NUM_FEATURES = 2
@@ -33,3 +38,83 @@ X_blob_train, X_blob_test, y_blob_train, y_blob_test = train_test_split(X_blob,
 plt.figure(figsize=(10, 7))
 plt.scatter(X_blob[:, 0], X_blob[:, 1], c=y_blob, cmap=plt.cm.RdYlBu);
 # plt.show()
+
+# Create an instance of BlobModel and send it to the target device
+model_4 = BlobModel(input_features=NUM_FEATURES,
+                    output_features=NUM_CLASSES,
+                    hidden_units=8)
+
+# Create loss and optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model_4.parameters(),
+                            lr=0.1)
+
+# Fit the model
+torch.manual_seed(42)
+
+# Set number of epochs
+epochs = 100
+torchmetrics_accuracy = Accuracy(task='multiclass', num_classes=4)
+# # Put data to target device
+# X_blob_train, y_blob_train = X_blob_train.to(device), y_blob_train.to(device)
+# X_blob_test, y_blob_test = X_blob_test.to(device), y_blob_test.to(device)
+
+for epoch in range(epochs):
+    ### Training
+    model_4.train()
+
+    # 1. Forward pass
+    y_logits = model_4(X_blob_train) # model outputs raw logits
+    y_pred = torch.softmax(y_logits, dim=1).argmax(dim=1) # go from logits -> prediction probabilities -> prediction labels
+    # print(y_logits)
+    # 2. Calculate loss and accuracy
+    loss = loss_fn(y_logits, y_blob_train)
+    acc = torchmetrics_accuracy(y_blob_train,
+                      y_pred)
+
+    # 3. Optimizer zero grad
+    optimizer.zero_grad()
+
+    # 4. Loss backwards
+    loss.backward()
+
+    # 5. Optimizer step
+    optimizer.step()
+
+    ### Testing
+    model_4.eval()
+    with torch.inference_mode():
+      # 1. Forward pass
+      test_logits = model_4(X_blob_test)
+      test_pred = torch.softmax(test_logits, dim=1).argmax(dim=1)
+      # 2. Calculate test loss and accuracy
+      test_loss = loss_fn(test_logits, y_blob_test)
+      test_acc = torchmetrics_accuracy(y_blob_test,
+                             test_pred)
+
+    # Print out what's happening
+    if epoch % 10 == 0:
+        print(f"Epoch: {epoch} | Loss: {loss:.5f}, Acc: {acc:.2f}% | Test Loss: {test_loss:.5f}, Test Acc: {test_acc:.2f}%")
+
+
+
+""" Visualizing the decision boundary"""
+
+# Download helper functions from Learn PyTorch repo (if not already downloaded)
+if Path("helper_functions.py").is_file():
+  print("helper_functions.py already exists, skipping download")
+else:
+  print("Downloading helper_functions.py")
+  request = requests.get("https://raw.githubusercontent.com/mrdbourke/pytorch-deep-learning/main/helper_functions.py")
+  with open("helper_functions.py", "wb") as f:
+    f.write(request.content)
+
+#
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.title("Train")
+plot_decision_boundary(model_4, X_blob_train, y_blob_train)
+plt.subplot(1, 2, 2)
+plt.title("Test")
+plot_decision_boundary(model_4, X_blob_test, y_blob_test)
+plt.show()
